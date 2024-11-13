824b565a4ac1723c0a8a03f2996b8e79c27f3413
diogolmousinho <diogolmousinho@gmail.com>
2024-11-13 16:49:48 +0000

    Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import MapComponent from './components/MapComponent';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <header className="flex items-center p-2 bg-white">
-        <img src="/NovaSpots_logo.jpg" alt="NovaSpots Logo" className="h-12 mr-2" />
-        <h1 className="text-2xl font-bold font-poppins" style={{ color: '#0463ba' }}>NovaSpots</h1>
-      </header>
+      <Header/>
       <div className="flex flex-col items-center mt-[-5]">
-        <h1 className="text-xl font-poppins mb-2">Campus Map</h1>
         <MapComponent />
       </div>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return <header className="flex items-center p-2 bg-white">
+        <img src="/NovaSpots_logo.jpg" alt="NovaSpots Logo" className="h-12 mr-2" />
+        <h1 className="text-2xl font-bold font-poppins" style={{ color: '#0463ba' }}>NovaSpots</h1>
+    </header>
+}
+
+export default Header;
