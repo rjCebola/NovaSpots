44f712aa892cb156dbb19efbb1496f8369a2729a
Mariana <mi.aguiar@campus.fct.unl.pt>
2024-11-13 15:58:02 +0000

    added logos & changed zoom controls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import MapComponent from './components/MapComponent';
+import './App.css';
 
 function App() {
   return (
     <>
-      <h1 className='text-center'>NovaSpots Campus Map</h1>
-      <MapComponent />
+      <header className='app-header'>
+        <img src='/NovaSpots_logo.jpg' alt='NovaSpots Logo' className='logo' />
+        <h1 className='app-name'>NovaSpots</h1>
+      </header>
+      <div className='map-container'>
+        <h1 className='map-name'>Campus Map</h1>
+        <MapComponent />
+      </div>
     </>
   );
 }
--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -3,21 +3,27 @@ import React from 'react';
 import { MapContainer, ImageOverlay } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import './MapComponent.css'; 
 
 const MapComponent = () => {
   // Define the bounds for the image overlay (latitude/longitude pairs)
-  const bounds = [[0, 0], [1509, 1665]]; // Adjust based on your image scale
+  const bounds = [[0, 0], [1665, 1509]]; // Adjust based on your image scale
 
   return (
     <MapContainer
       center={[754.5, 832.5]} // Center point of the image; adjust as needed
-      zoom={2} // Adjust zoom level as needed for optimal display
-      style={{ height: '100vh', width: '100%' }}
+      zoom={-1} // Adjust zoom level as needed for optimal display
+      style={{ height: '100vh', width: '100%', backgroundColor: 'white'}}
       crs={L.CRS.Simple} // Use a simple coordinate reference system for custom images
+      maxBounds={bounds} 
+      minZoom={-2}
+      maxZoom={2}
+      bounds={bounds}
     >
       <ImageOverlay
         url="/campus.jpg" // Ensure this path is correct and accessible
         bounds={bounds}
+        style={{ width: '100%', height: '100%', objectFit: 'contain' }} // Maintain aspect ratio
       />
     </MapContainer>
   );
