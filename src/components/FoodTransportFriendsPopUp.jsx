b42421f36d35b056f8b9e395587ca461102ac410
rj.cebola <rj.cebola@campus.fct.unl.pt>
2024-11-14 13:39:12 +0000

    layers popup slider bar

--- src/components/FoodTransportFriendsPopUp.jsx
+++ src/components/FoodTransportFriendsPopUp.jsx
@@ -20,19 +20,22 @@ const LayersPopUp = ({setState}) => {
 
 
   return (
-    <SliderAnimation showing={!closing} className="fixed bottom-0 w-full z-[999] bg-white p-4 rounded-t-3xl flex h-1/6 justify-around items-center shadow-2xl shadow-black">
-      <button onClick={() => handleClose()} className={buttonStyle} >
-        <FontAwesomeIcon icon={faUtensils} size="2x"/>
-        <span className="mt-1 text-sm">Food</span>
-      </button>
-      <button onClick={() => handleClose()} className={buttonStyle} >
-        <FontAwesomeIcon icon={faTrain} size="2x"/>
-        <span className="mt-1 text-sm">Transport</span>
-      </button>
-      <button onClick={() => handleClose()} className={buttonStyle} >
-        <FontAwesomeIcon icon={faUserGroup} size="2x"/>
-        <span className="mt-1 text-sm">Friends</span>
-      </button>
+    <SliderAnimation showing={!closing} className="fixed bottom-0 w-full z-[999] bg-white p-4 pt-2 rounded-t-3xl flex flex-col justify-between items-center shadow-2xl shadow-black group">
+      <div className="w-1/4 h-1 bg-gray-300 mb-4 group-active:bg-gray-600"></div>
+      <div className="flex justify-around w-full">
+        <button onClick={() => handleClose()} className={buttonStyle} >
+          <FontAwesomeIcon icon={faUtensils} size="2x"/>
+          <span className="mt-1 text-sm">Food</span>
+        </button>
+        <button onClick={() => handleClose()} className={buttonStyle} >
+          <FontAwesomeIcon icon={faTrain} size="2x"/>
+          <span className="mt-1 text-sm">Transport</span>
+        </button>
+        <button onClick={() => handleClose()} className={buttonStyle} >
+          <FontAwesomeIcon icon={faUserGroup} size="2x"/>
+          <span className="mt-1 text-sm">Friends</span>
+        </button>
+      </div>
     </SliderAnimation>
   );
 };
