7d8d5a8f7287a076814313ac10e6ace04ad81d81
miguel-estudante <redminer03@gmail.com>
2024-11-21 23:12:40 +0000

    animation for settings and button feedback idea

--- src/components/SettingsPopUp.jsx
+++ src/components/SettingsPopUp.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
+
+
 function SettingsPopUp({ setState, setViewProfile }) {
 
-
-
+  const [scale,setScale] = useState('scale(0)');
+  useEffect(() => {
+    setScale('scale(1)');
+    return() => {
+      setScale('scale(0)')
+      setTimeout(() => {
+      },300);
+    }
+  }, []);
+  
   return (
-    <div className="absolute top-16 right-0 w-32 bg-white border border-gray-200 rounded-bl-lg shadow-lg p-2 z-[999]">
+    <div className="absolute top-16 right-0 w-32 bg-white border border-gray-200 rounded-bl-lg shadow-lg p-2 z-[999]"
+      style={{
+        transform : scale,
+        transformOrigin : 'top right',
+        transition: 'transform 0.1s ease-out' 
+      }}
+    >
       <button className="block w-full text-lg text-left px-3 py-2">Profile</button>
       <button className="block w-full text-lg text-left px-3 py-2" onClick={() => { setState("friends"); setViewProfile(false) }}>Friends</button>
       <button className="block w-full text-lg text-left px-3 py-2">Settings</button>
